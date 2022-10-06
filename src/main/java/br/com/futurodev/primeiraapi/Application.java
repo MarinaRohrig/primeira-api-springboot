b66b5013bd9e77316679353b81b9a36a6bdfebef
Mah <marinalarissacarpesrohrig@gmail.com>
2022-10-05 21:24:34 -0300

    + comentários no application.java

--- src/main/java/br/com/futurodev/primeiraapi/Application.java
+++ src/main/java/br/com/futurodev/primeiraapi/Application.java
@@ -10,6 +10,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class Application {
     public static void main(String[] args) {
+
+        // Roda o projeto java spring boot , recebe uma classe
         SpringApplication.run(Application.class, args);
     }
 }
