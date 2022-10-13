8df87bdf21614e31c0eeb31b50d098a4f494cada
Mah <marinalarissacarpesrohrig@gmail.com>
2022-10-13 08:56:49 -0300

    criacao usuariomodel

--- src/main/java/br/com/futurodev/primeiraapi/models/UsuarioModel.java
+++ src/main/java/br/com/futurodev/primeiraapi/models/UsuarioModel.java
@@ -1,4 +1,9 @@
 package br.com.futurodev.primeiraapi.models;
 
 public class UsuarioModel {
+    private Long id;
+    private String login;
+    private String senha;
+    private String nome;
+
 }
