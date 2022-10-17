cd2168e16b7fa46862a4199ab77512685a049260
Mah <marinalarissacarpesrohrig@gmail.com>
2022-10-17 20:27:49 -0300

    método delete no UsuarioController

--- src/main/java/br/com/futurodev/primeiraapi/controllers/UsuarioController.java
+++ src/main/java/br/com/futurodev/primeiraapi/controllers/UsuarioController.java
@@ -2,13 +2,11 @@ package br.com.futurodev.primeiraapi.controllers;
 
 import br.com.futurodev.primeiraapi.models.UsuarioModel;
 import br.com.futurodev.primeiraapi.repository.UsuarioRepository;
+import org.apache.coyote.Response;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping(value = "/usuario")
@@ -23,7 +21,20 @@ public class UsuarioController {
     @PostMapping(value = "/", produces= "application/json") // quando fizer uma requisição localhost:8000/usuario e usar o POST, cai aqui
     public ResponseEntity<UsuarioModel> cadastrar(@RequestBody UsuarioModel usuario){ // recebe o json da requisição e converte para um objeto UsuarioModel
         UsuarioModel usu = usuarioRepository.save(usuario);
-        return new ResponseEntity<UsuarioModel>(usu, HttpStatus.OK);
+        return new ResponseEntity<UsuarioModel>(usu, HttpStatus.CREATED);
+    }
+
+    @PutMapping(value="/",produces= "application/json")
+    public ResponseEntity<UsuarioModel> atualizar(@RequestBody UsuarioModel usuario){
+        UsuarioModel usu = usuarioRepository.save(usuario);
+        return new ResponseEntity<UsuarioModel>(usu,HttpStatus.OK);
+    }
+
+    @DeleteMapping(value="/")
+    @ResponseBody
+    public ResponseEntity<String> delete(@RequestParam Long idUsuario){
+        usuarioRepository.deleteById(idUsuario);
+        return new ResponseEntity<String>("Usuário deletado com sucesso!", HttpStatus.OK);
     }
 
 
