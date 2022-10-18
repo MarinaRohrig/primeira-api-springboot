c87dcaa47a8473142ed660be7e133611403cc676
Mah <marinalarissacarpesrohrig@gmail.com>
2022-10-18 15:06:06 -0300

    Adição do import.sql

---
+++ src/main/resources/import.sql
@@ -0,0 +1,6 @@
+INSERT INTO public.produto(descricao, preco_unitario) VALUES ('Arroz Tio Joao','5.40');
+INSERT INTO public.produto(descricao, preco_unitario) VALUES ('Macarrão','8.00');
+INSERT INTO public.produto(descricao, preco_unitario) VALUES ('Batata','2.00');
+INSERT INTO public.produto(descricao, preco_unitario) VALUES ('Erva Doce','10.45');
+INSERT INTO public.produto(descricao, preco_unitario) VALUES ('Açúcar','8.00');
+INSERT INTO public.produto(descricao, preco_unitario) VALUES ('Laranja Doce','20.30');
