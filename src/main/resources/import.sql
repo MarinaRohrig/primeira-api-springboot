077305ea4146074afe25fdde6b083347e375be1f
Mah <marinalarissacarpesrohrig@gmail.com>
2022-10-18 19:24:45 -0300

    criado import de usuario e dtoos

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -4,3 +4,10 @@ INSERT INTO public.produto(descricao, preco_unitario) VALUES ('Batata','2.00');
 INSERT INTO public.produto(descricao, preco_unitario) VALUES ('Erva Doce','10.45');
 INSERT INTO public.produto(descricao, preco_unitario) VALUES ('Açúcar','8.00');
 INSERT INTO public.produto(descricao, preco_unitario) VALUES ('Laranja Doce','20.30');
+
+
+INSERT INTO public.usuario(login, nome, senha) VALUES ( 'marinalarissa', 'Marina Larissa', '102030');
+INSERT INTO public.usuario(login, nome, senha) VALUES ( 'larissarohrig', 'Larissa Marina', '302010');
+INSERT INTO public.usuario(login, nome, senha) VALUES ( 'Tutu', 'Testezinho', '10000');
+INSERT INTO public.usuario(login, nome, senha) VALUES ( 'Lalala', 'Lalalala', '235523');
+INSERT INTO public.usuario(login, nome, senha) VALUES ( 'Luluzinha', 'Luciana', '15523');
